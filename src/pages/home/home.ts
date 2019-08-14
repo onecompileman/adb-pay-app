db1fd2bfc32c7b5ccd7e882d7a29442e8a059555
onecompileman <stephen.vinuya@gmail.com>
2019-08-14 19:28:49 +0800

    feat: added home-page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { RegistrationPage } from '../registration/registration';
 
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
 
+  ionViewDidLoad() {
+
+  }
+
+  navigateToRegistration() {
+    this.navCtrl.push(RegistrationPage);
   }
 
 }
